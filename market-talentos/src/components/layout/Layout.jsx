b743cc6d058fba4c2c31d44288bf06a277e96d74
vladie777 <diego.800luis@gmail.com>
2023-07-05 12:47:22 -0500

    creacion y edicion del home

--- market-talentos/src/components/layout/Layout.jsx
+++ market-talentos/src/components/layout/Layout.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import LogoMakaia from "../../assets/makaia.png";
+import "./styledLayout.scss";
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+  const links = [
+    {
+      id: 1,
+      name: "Somos MAKAIA",
+    },
+    {
+      id: 2,
+      name: "Contactanos",
+    },
+    {
+      id: 3,
+      name: "Experiencias",
+    },
+    {
+      id: 4,
+      name: "Nuevo Talento",
+    },
+    {
+      id: 5,
+      name: "Informacion interes",
+    },
+    {
+      id: 6,
+      name: "Blog",
+    },
+  ];
 
-export default Layout
+  return (
+    <div>
+        <figure className="header__logomakaia">
+          <img src={LogoMakaia} alt="logo de la empresa" />
+        </figure>
+      <header className="header">
+        <div className="header__navbar">
+          <div className="header__navbarlink">
+            {links.map((link) => (
+              <button className="header__button" key={link.id}>
+                {link.name}
+              </button>
+            ))}
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Layout;
