4bd83d295e882139acd646a391bd7c31900da6dd
diana pinzon <diaphi@hotmail.com>
2023-07-05 12:15:39 -0500

    cambios Edit Profile

--- market-talentos/src/pages/EditProfile.jsx
+++ market-talentos/src/pages/EditProfile.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/styleEditProfile.scss';
 import imgTalent from '../assets/elisa.jpeg';
 import useOnClick from '../funtions/useOnClick';
 
 const EditProfile = () => {
+  const [profile, setProfile] = useState('');
+
+  const handleChangeProfile = (event) => {
+    // limita a 160 palabras
+    const inputText = event.target.value;
+    const words = inputText.split(' ');
+    if (words.length <= 160) {
+      setProfile(inputText);
+    }
+  };
 
   const handleClick = useOnClick();
 
-  const personalDate =[
-    {
-      id: 1,
-      name: "",
-      type: "text",
-      placeHolder: "ingrese sus nombres",
-      className: "",
-    }
-  ]
   return (
     <>
       <section className='editProfile'>
@@ -27,40 +28,76 @@ const EditProfile = () => {
               </button>
             </div>
             <div className='editProfile__container-infoCustom'>
-
               <div className='editProfile__container-infoContacts'>
-                <div className='editProfile__container-imgTalent'
-                  onClick={() => handleClick("editImgProfile", "")}>
+                <div
+                  className='editProfile__container-imgTalent'
+                  onClick={() => handleClick('editImgProfile', '')}
+                >
                   <figure className='editProfile__card-figure'>
-                    <img src={imgTalent} alt="imgTalent" />
+                    <img src={imgTalent} alt='imgTalent' />
                   </figure>
                 </div>
               </div>
               <div className='editProfile__container-profile'>
-                <input type="email" name= 'inputGitUp' id="inputGitUp"
-                  className="editProfile__input"
-                  placeholder="ingresa el vinculo de tu cuenta en Git Up" />
+                <input
+                  type='email'
+                  name='inputGitUp'
+                  id='inputGithub'
+                  className='editProfile__input'
+                  placeholder='Ingresa el vínculo de tu cuenta en Github'
+                />
               </div>
-              <div className="editProfile__knowLedge">
-                <label cclassNamelass="editProfile__knowLedge">Conocimientos</label>
-                <div className="editProfile__">
-                  <input type="checkbox" className="form-check-imput" name="knowledge" id="python" value="python" />
-                  <label for="python" className="form-check-label">python</label>  
+              <div className='editProfile__container-profile'>
+                <input
+                  type='email'
+                  name='inputlinkedin'
+                  id='inputlinkedin'
+                  className='editProfile__input'
+                  placeholder='Ingresa el vínculo de tu cuenta en Linkedin'
+                />
+              </div>
+              <div className='editProfile__knowLedge'>
+                <label className='editProfile__knowLedge'>Conocimientos</label>
+                <div className='editProfile__'>
+                  <input
+                    type='checkbox'
+                    className='form-check-imput'
+                    name='knowledge'
+                    id='python'
+                    value='python'
+                  />
+                  <label htmlFor='python' className='form-check-label'>
+                    Python
+                  </label>
                 </div>
-            </div>
+              </div>
               <div className='editProfile__container-custom'>
-                <button className='editProfile__button'
-                  onClick={() => handleClick("next", "")}
-                >Guardar</button>
-
+                <label htmlFor='profile' className='editProfile__profile-label'> 
+                </label>
+                <textarea
+                  id='profile'
+                  name='profile'
+                  className='editProfile__input-profile'
+                  placeholder='Escribe aquí tu presentación Presentación (160 palabras máximo):'
+                  value={profile}
+                  onChange={handleChangeProfile}
+                />
+              </div>
+          
+              <div className='editProfile__container-custom'>
+                <button
+                  className='editProfile__button'
+                  onClick={() => handleClick('next', '')}
+                >
+                  Guardar
+                </button>
               </div>
             </div>
           </section>
         </div>
       </section>
-
     </>
-  )
-}
+  );
+};
 
 export default EditProfile;
