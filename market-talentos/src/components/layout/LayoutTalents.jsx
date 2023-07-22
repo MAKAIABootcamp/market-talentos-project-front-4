777519716630c5e41caa5480e399737d4e785410
diana pinzon <diaphi@hotmail.com>
2023-07-22 18:03:51 -0500

    Menu hamburguesa

--- market-talentos/src/components/layout/LayoutTalents.jsx
+++ market-talentos/src/components/layout/LayoutTalents.jsx
@@ -9,15 +9,23 @@ import imgReturn from "../../assets/arrowleft.png"
 import { NavLink, useNavigate } from 'react-router-dom';
 import { singOutAsync } from '../../redux/actions/usersActions';
 import { useDispatch } from 'react-redux';
+import { Drawer, IconButton, List, ListItem, ListItemText } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const LayoutTalents = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [showExitButton, setShowExitButton] = useState(false);
+  const [menuAbierto, setMenuAbierto] = useState(false);
 
   const handleToggleExitButton = () => {
     setShowExitButton(!showExitButton);
   }
+
+  const toggleMenu = () => {
+    console.log('Se hizo clic en el botón');
+    setMenuAbierto(!menuAbierto);
+  };
 
  
   // const handleExit = () => {
@@ -53,62 +61,177 @@ const LayoutTalents = () => {
 
   return (
     <>
-      <header className="layoutTalent">
-        <div className='layoutTalent__container-imgReturn'>
-          <figure
-            onClick={() => navigate("/")}
-            className="layoutTalent__figure-imgReturn"
-          >
-            <img src={imgReturn} alt="imgReturn" />
-          </figure>
-        </div>
-        <div className="layoutTalent__container-logo">
-          <figure className="layoutTalent__figure-logo">
-            <img src={LogoMakaia} alt="logo de la empresa" />
-          </figure>
-        </div>
-        <div className="layoutTalent__container-navLink">
-          <ul className='layoutTalent__navbar-ul'>
+    <header className="layoutTalent">
+      <div className='layoutTalent__container-imgReturn'>
+        <figure
+          onClick={() => navigate("/")}
+          className="layoutTalent__figure-imgReturn"
+        >
+          <img src={imgReturn} alt="imgReturn" />
+        </figure>
+      </div>
+      <div className="layoutTalent__container-logo">
+        <figure className="layoutTalent__figure-logo">
+          <img src={LogoMakaia} alt="logo de la empresa" />
+        </figure>
+      </div>
+
+      
+      
+     
+      <div className="layoutTalent__container-navLink">
+        <IconButton 
+          edge="right"
+          color="inherit"
+          aria-label="menu"
+          onClick={toggleMenu}
+          
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          anchor="right"
+          open={menuAbierto}
+          onClose={() => setMenuAbierto(false)}
+        >
+          <List>
             {products.map((item) => (
-              <li key={item.id}>
-                
-                <NavLink className={"layoutTalent__link"} to={item.path}>{item.name}</NavLink>
-                {/* <img src={item.img} alt={item.name} /> */}
-              </li>
-              
-            ))}
-          </ul>
-        </div>
-        <div className='layoutTalent__container-login'>
-          <div>
-          <img  className='layoutTalent__chatbot'src={chatbot} alt="chatbot" />
-          </div>
-          <div>
-          <img  className='layoutTalent__notification'src={notification} alt="notification" />
-          </div>
-          <div
-            className='layoutTalent_container-imgTalent'
-            onClick={handleToggleExitButton}
-            onMouseEnter={() => setShowExitButton(true)}
-            onMouseLeave={() => setShowExitButton(false)}
-          >
-            <figure className='layoutTalent__card-figure'>
-              <img src={usuario} alt="imgTalent"   onClick={() => dispatch(singOutAsync())} />
-            </figure>
-            {/* Mostrar el botón de "Salida" (Exit) solo cuando se hace clic en la imagen de usuario */}
-            {showExitButton && (
-              <button
-                className='layoutTalent__button-exit'
-              
+              <ListItem
+               
+                key={item.id}
+                component={NavLink}
+                to={item.path}
+                onClick={() => setMenuAbierto(false)}
               >
-                Salir
-              </button>
-            )}
-          </div>
-       
+                <ListItemText primary={item.name} />
+              </ListItem>
+            ))}
+          </List>
+          <div className='layoutTalent__container-login'>
+        <div>
+          <img className='layoutTalent__chatbot' src={chatbot} alt="chatbot" />
         </div>
-      </header>
-    </>
+        <div>
+          <img className='layoutTalent__notification' src={notification} alt="notification" />
+        </div>
+        <div
+          className='layoutTalent_container-imgTalent'
+          onClick={handleToggleExitButton}
+          onMouseEnter={() => setShowExitButton(true)}
+          onMouseLeave={() => setShowExitButton(false)}
+        >
+          <figure className='layoutTalent__card-figure'>
+            <img src={usuario} alt="imgTalent" onClick={() => dispatch(singOutAsync())} />
+          </figure>
+          {/* Mostrar el botón de "Salida" (Exit) solo cuando se hace clic en la imagen de usuario */}
+          {showExitButton && (
+            <button
+              className='layoutTalent__button-exit'
+            >
+              Salir
+            </button>
+          )}
+        </div>
+      </div>
+        </Drawer>
+
+        <ul className='layoutTalent__navbar-ul'>
+          {products.map((item) => (
+            <li key={item.id}>
+              <NavLink className={"layoutTalent__link"} to={item.path}>{item.name}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+   
+
+
+      <div className='layoutTalent__container-login'>
+        <div>
+          <img className='layoutTalent__chatbot' src={chatbot} alt="chatbot" />
+        </div>
+        <div>
+          <img className='layoutTalent__notification' src={notification} alt="notification" />
+        </div>
+        <div
+          className='layoutTalent_container-imgTalent'
+          onClick={handleToggleExitButton}
+          onMouseEnter={() => setShowExitButton(true)}
+          onMouseLeave={() => setShowExitButton(false)}
+        >
+          <figure className='layoutTalent__card-figure'>
+            <img src={usuario} alt="imgTalent" onClick={() => dispatch(singOutAsync())} />
+          </figure>
+          {/* Mostrar el botón de "Salida" (Exit) solo cuando se hace clic en la imagen de usuario */}
+          {showExitButton && (
+            <button
+              className='layoutTalent__button-exit'
+            >
+              Salir
+            </button>
+          )}
+        </div>
+      </div>
+    </header>
+  </>
+    // <>
+    //   <header className="layoutTalent">
+    //     <div className='layoutTalent__container-imgReturn'>
+    //       <figure
+    //         onClick={() => navigate("/")}
+    //         className="layoutTalent__figure-imgReturn"
+    //       >
+    //         <img src={imgReturn} alt="imgReturn" />
+    //       </figure>
+    //     </div>
+    //     <div className="layoutTalent__container-logo">
+    //       <figure className="layoutTalent__figure-logo">
+    //         <img src={LogoMakaia} alt="logo de la empresa" />
+    //       </figure>
+    //     </div>
+    //     <div className="layoutTalent__container-navLink">
+    //       <ul className='layoutTalent__navbar-ul'>
+    //         {products.map((item) => (
+    //           <li key={item.id}>
+                
+    //             <NavLink className={"layoutTalent__link"} to={item.path}>{item.name}</NavLink>
+    //             {/* <img src={item.img} alt={item.name} /> */}
+    //           </li>
+              
+    //         ))}
+    //       </ul>
+    //     </div>
+    //     <div className='layoutTalent__container-login'>
+    //       <div>
+    //       <img  className='layoutTalent__chatbot'src={chatbot} alt="chatbot" />
+    //       </div>
+    //       <div>
+    //       <img  className='layoutTalent__notification'src={notification} alt="notification" />
+    //       </div>
+    //       <div
+    //         className='layoutTalent_container-imgTalent'
+    //         onClick={handleToggleExitButton}
+    //         onMouseEnter={() => setShowExitButton(true)}
+    //         onMouseLeave={() => setShowExitButton(false)}
+    //       >
+    //         <figure className='layoutTalent__card-figure'>
+    //           <img src={usuario} alt="imgTalent"   onClick={() => dispatch(singOutAsync())} />
+    //         </figure>
+    //         {/* Mostrar el botón de "Salida" (Exit) solo cuando se hace clic en la imagen de usuario */}
+    //         {showExitButton && (
+    //           <button
+    //             className='layoutTalent__button-exit'
+              
+    //           >
+    //             Salir
+    //           </button>
+    //         )}
+    //       </div>
+       
+    //     </div>
+    //   </header>
+      
+    // </>
   )
 }
 
