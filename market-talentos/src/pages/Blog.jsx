86efb9531a105e78135eff6774ebce1379e7a9d2
diego <diego12luisgodi@gmail.com>
2023-07-23 14:08:57 -0500

    blog

--- market-talentos/src/pages/Blog.jsx
+++ market-talentos/src/pages/Blog.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import LayoutHome from "../components/layout/LayoutHome";
+import Footer from "../components/footer/Footer";
+import "../style/styleBlog.scss";
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <>
+      <LayoutHome />
+      <main className="blog">
+        <div className="blog__containerinfo1">
+          <div className="blog__textinformation">
+            <h1>Aprende algunos consejos de programación aquí.</h1>
+          </div>
+          <div className="blog__imagen">
+            <img
+              src="https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default Blog
+export default Blog;
