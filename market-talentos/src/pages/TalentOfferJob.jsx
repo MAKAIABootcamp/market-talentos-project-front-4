8c5fe8b7dc9ee3b531b4a784373969c3c9cb7844
diego <diego12luisgodi@gmail.com>
2023-07-24 22:52:04 -0500

    error

--- market-talentos/src/pages/TalentOfferJob.jsx
+++ market-talentos/src/pages/TalentOfferJob.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../style/styleTalentOfferJob.scss";
 import { NavLink, useNavigate } from "react-router-dom";
 import imgTalent from "../assets/elisa.jpeg";
