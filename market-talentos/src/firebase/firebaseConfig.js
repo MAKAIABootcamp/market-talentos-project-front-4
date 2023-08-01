6be8506f943059ed65c6295f8f4e5ea52743a920
startcroft <santiagogomezpavas42@gmail.com>
2023-08-01 09:01:55 -0500

    cambiando la configuracion de firebase

--- market-talentos/src/firebase/firebaseConfig.js
+++ market-talentos/src/firebase/firebaseConfig.js
@@ -8,13 +8,12 @@ import {getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBKlScJqh0l3MdqbfvQlTv7I4LmzUEuk90",
-  authDomain: "marketplace-talents.firebaseapp.com",
-  projectId: "marketplace-talents",
-  storageBucket: "marketplace-talents.appspot.com",
-  messagingSenderId: "258198592898",
-  appId: "1:258198592898:web:b791c1da32f9c5f0e9839b",
-  measurementId: "G-32TS1DFJWE"
+  apiKey: "AIzaSyALNNG-zuo6Qce6rQvBND3aksoCEZe5eDc",
+  authDomain: "market-place-talentos.firebaseapp.com",
+  projectId: "market-place-talentos",
+  storageBucket: "market-place-talentos.appspot.com",
+  messagingSenderId: "784638244005",
+  appId: "1:784638244005:web:c576a336823c28e24a494c"
 };
 
 // Initialize Firebase
